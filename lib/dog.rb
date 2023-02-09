1cdb06b80a56ccb2650a611e0977a7029158bdf8
Brian <wamwangimathenge@gmail.com>
2023-02-09 18:06:14 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+    # Name Getter method
+    def name
+        @name
+    end
+
+    # Name Setter method
+    def name=(name)
+        @name = name
+    end
+
+    # Breed Getter method
+    def breed
+        @breed
+    end
+
+    # Breed Setter method
+    def breed=(breed)
+        @breed = breed
+    end
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+    # Name Getter method
+    def name
+        @name
+    end
+
+    # Name Setter method
+    def name=(name)
+        @name = name
+    end
+
+    # Job Getter Method
+    def job
+        @job
+    end
+
+    # Job Setter Method
+    def job=(job)
+        @job = job
+    end
+
 end
